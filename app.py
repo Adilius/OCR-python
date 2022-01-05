35e1becc06b3332a2fde5f0c87703fdf2cfe9f88
Adilius <leugue-gamer@hotmail.com>
2022-01-05 22:51:43 +0100

    Added main function and utils script

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import preprocessing
+import text_detection
 import util
 import cv2
 
 # Input image
 image = cv2.imread("image.png", cv2.IMREAD_COLOR)
 util.show_image(image)
+
 # Preprocess
 image = preprocessing.preprocess_image(
     image=image, grayscale=True, noise_removal=True, deskew=True, normalize=True
 )
 util.show_image(image)
-cv2.imwrite('preprocessed.png', 255*image)
+
+image = cv2.merge([image,image,image])
+
+# Text detection
+image, final_boxes = text_detection.detect_text(image, dimensions=True, threshold = 0.1, overlap_threshold = 0.5)
+
+util.show_image(image)
